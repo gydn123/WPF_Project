b88eee5c4718685db1ba6a859cdec8d25f323997
gydn123 <gydn1234@gmail.com>
2025-01-04 17:39:09 +0900

    [feature] 첫번째 커밋

---
+++ FirstProject1/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstProject1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show("알림 띄우기");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyLabel.Content = "내용 변경";
+        }
+    }
+}
